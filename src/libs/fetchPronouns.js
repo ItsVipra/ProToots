40086a5ade0596cd4d72d0f287082d64b35b54ba
nachtjasmin <nachtjasmin@posteo.de>
2023-06-18 19:22:52 +0200

    Fix all the code issues right now

--- src/libs/fetchPronouns.js
+++ src/libs/fetchPronouns.js
@@ -1,21 +1,24 @@
-import { info } from "./logging";
+import { debug, error, info, warn } from "./logging";
 import { cachePronouns } from "./caching";
+import { storage } from "webextension-polyfill";
+
+const cacheMaxAge = 24 * 60 * 60 * 1000; // time after which cached pronouns should be checked again: 24h
 
 /**
  * Fetches pronouns associated with account name.
  * If cache misses status is fetched from the instance.
  *
  * @param {string | undefined} statusID ID of the status being requested, in case cache misses.
- * @param {string} account_name The account name, used for caching. Should have the "@" prefix.
+ * @param {string} accountName The account name, used for caching. Should have the "@" prefix.
  */
-export async function fetchPronouns(statusID, account_name) {
+export async function fetchPronouns(statusID, accountName) {
 	// log(`searching for ${account_name}`);
-	let cacheResult = { pronounsCache: {} };
+	let cacheResult = {};
 	try {
 		cacheResult = await storage.local.get();
 		if (!cacheResult.pronounsCache) {
 			//if result doesn't have "pronounsCache" create it
-			let pronounsCache = {};
+			const pronounsCache = {};
 			await storage.local.set({ pronounsCache });
 			cacheResult = { pronounsCache: {} };
 		}
@@ -24,32 +27,32 @@ export async function fetchPronouns(statusID, account_name) {
 		// ignore errors, we have an empty object as fallback.
 	}
 	// Extract the current cache by using object destructuring.
-	if (account_name in cacheResult.pronounsCache) {
-		let { value, timestamp } = cacheResult.pronounsCache[account_name];
+	if (accountName in cacheResult.pronounsCache) {
+		const { value, timestamp } = cacheResult.pronounsCache[accountName];
 
 		// If we have a cached value and it's not outdated, use it.
-		if (value && Date.now() - timestamp < max_age) {
-			info(`${account_name} in cache with value: ${value}`);
+		if (value && Date.now() - timestamp < cacheMaxAge) {
+			info(`${accountName} in cache with value: ${value}`);
 			return value;
 		}
 	}
 
-	info(`${account_name} cache entry is stale, refreshing`);
+	info(`${accountName} cache entry is stale, refreshing`);
 
 	if (!statusID) {
-		console.warn(
-			`Could not fetch pronouns for user ${account_name}, because no status ID was passed. This is an issue we're working on.`,
+		warn(
+			`Could not fetch pronouns for user ${accountName}, because no status ID was passed. This is an issue we're working on.`,
 		);
-		return;
+		return null;
 	}
 
-	info(`${account_name} not in cache, fetching status`);
-	let status = await fetchStatus(statusID);
+	info(`${accountName} not in cache, fetching status`);
+	const status = await fetchStatus(statusID);
 
-	let PronounField = getPronounField(status, account_name);
+	const PronounField = getPronounField(status, accountName);
 	if (PronounField == "null") {
 		//TODO: if no field check bio
-		info(`no pronouns found for ${account_name}, cached null`);
+		info(`no pronouns found for ${accountName}, cached null`);
 	}
 	return PronounField;
 }
@@ -82,29 +85,29 @@ async function fetchStatus(statusID) {
  * If found returns the value of said field.
  *
  * @param {string} status
- * @param {string} account_name
+ * @param {string} accountName
  * @returns {string} Author pronouns if found. Otherwise returns "null"
  */
-function getPronounField(status, account_name) {
+function getPronounField(status, accountName) {
 	debug(status);
 	// get account from status and pull out fields
-	let account = status["account"];
-	let fields = account["fields"];
+	const account = status.account;
+	const fields = account.fields;
 
-	for (let field of fields) {
+	for (const field of fields) {
 		//match fields against "pronouns"
 		//TODO: multiple languages
-		if (field["name"].toLowerCase().includes("pronouns")) {
-			debug(`${account["acct"]}: ${field["value"]}`);
+		if (field.name.toLowerCase().includes("pronouns")) {
+			debug(`${account.acct}: ${field.value}`);
 
-			cachePronouns(account_name, field["value"]);
-			return field["value"];
+			cachePronouns(accountName, field.value);
+			return field.value;
 		}
 	}
 
 	//if not returned by this point no field with pronouns was found
 
-	cachePronouns(account_name, "null");
+	cachePronouns(accountName, "null");
 	return "null";
 }
 
