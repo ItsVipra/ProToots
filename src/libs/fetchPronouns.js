3e6f52d5c2b62901f76672784f0265f85bce1755
Vipra <vivienvielva@gmail.com>
2023-06-29 20:29:39 +0200

    add support for more contexts
    this closes #25, #26, #27, #23,

--- src/libs/fetchPronouns.js
+++ src/libs/fetchPronouns.js
@@ -1,19 +1,21 @@
-import { debug, error, info, log, warn } from "./logging";
+import { debug, error, info, warn } from "./logging";
 import { cachePronouns, getPronouns } from "./caching";
+import { normaliseAccountName } from "./protootshelpers";
 
 const cacheMaxAge = 24 * 60 * 60 * 1000; // time after which cached pronouns should be checked again: 24h
 
 /**
  * Fetches pronouns associated with account name.
- * If cache misses status is fetched from the instance.
+ * If cache misses object is fetched from the instance.
  *
- * @param {string | undefined} statusID ID of the status being requested, in case cache misses.
+ * @param {string | undefined} dataID ID of the object being requested, in case cache misses.
  * @param {string} accountName The account name, used for caching. Should have the "@" prefix.
+ * @param {string} type Type of data-id
  */
-export async function fetchPronouns(statusID, accountName, type) {
+export async function fetchPronouns(dataID, accountName, type) {
 	// log(`searching for ${account_name}`);
 	const cacheResult = await getPronouns();
-	log(cacheResult);
+	debug(cacheResult);
 	// Extract the current cache by using object destructuring.
 	if (accountName in cacheResult.pronounsCache) {
 		const { value, timestamp } = cacheResult.pronounsCache[accountName];
@@ -29,20 +31,31 @@ export async function fetchPronouns(statusID, accountName, type) {
 
 	info(`${accountName} cache entry is stale, refreshing`);
 
-	if (!statusID) {
-		warn(
-			`Could not fetch pronouns for user ${accountName}, because no status ID was passed. This is an issue we're working on.`,
-		);
+	if (!dataID) {
+		warn(`Could not fetch pronouns for user ${accountName}, because no status ID was passed.`);
 		return null;
 	}
 
 	info(`${accountName} not in cache, fetching status`);
-	// const status = await fetchStatus(statusID);
+
 	let status;
 	if (type === "notification") {
-		status = await fetchNotification(statusID);
+		status = await fetchNotification(dataID);
+	} else if (type === "account") {
+		status = await fetchAccount(dataID);
+	} else if (type === "conversation") {
+		const conversations = await fetchConversations();
+		for (const conversation of conversations) {
+			for (const account of conversation.accounts) {
+				//conversations can have multiple participants, check that we're passing along the right account
+				if (normaliseAccountName(account.acct) == accountName) {
+					//package the account object in an empty object for compatibility with getPronounField()
+					status = { account: account };
+				}
+			}
+		}
 	} else {
-		status = await fetchStatus(statusID);
+		status = await fetchStatus(dataID);
 	}
 
 	const PronounField = getPronounField(status, accountName);
@@ -76,6 +89,12 @@ async function fetchStatus(statusID) {
 	return status;
 }
 
+/**
+ * Fetches notification by notificationID from host_name with user's access token.
+ *
+ * @param {string} notificationID ID of notification being requested.
+ * @returns {Promise<object>} Contents of notification in json form.
+ */
 async function fetchNotification(notificationID) {
 	const accessToken = await getActiveAccessToken();
 
@@ -88,11 +107,52 @@ async function fetchNotification(notificationID) {
 
 	const notification = await response.json();
 
-	const actioner = notification.account; //person who performed notification action
-
-	debug(notification);
-
 	return notification;
+}
+
+/**
+ * Fetches account by accountID from host_name with user's access token.
+ *
+ * @param {string} accountID ID of account being requested.
+ * @returns {Promise<object>} Contents of account in json form.
+ */
+async function fetchAccount(accountID) {
+	const accessToken = await getActiveAccessToken();
+
+	const response = await fetch(
+		`${location.protocol}//${location.host}/api/v1/accounts/${accountID}`,
+		{
+			headers: { Authorization: `Bearer ${accessToken}` },
+		},
+	);
+
+	const account = await response.json();
+
+	return { account: account };
+}
+
+/**
+ * Fetches the user's last <=40 direct message threads from host_name with user's access token.
+ * @returns {Promise<Array>} Array containing direct message thread objects in json from.
+ *
+ * DOCS: https://docs.joinmastodon.org/methods/conversations/#response
+ */
+async function fetchConversations() {
+	//the api wants status IDs, not conversation IDs
+	//as a result we can only get pronouns for the first 40 conversations max
+	//most of these should be in cache anyways
+	const accessToken = await getActiveAccessToken();
+
+	const response = await fetch(
+		`${location.protocol}//${location.host}/api/v1/conversations?limit=40`,
+		{
+			headers: { Authorization: `Bearer ${accessToken}` },
+		},
+	);
+
+	const conversations = await response.json();
+
+	return conversations;
 }
 
 /**
@@ -104,15 +164,15 @@ async function fetchNotification(notificationID) {
  * @returns {string} Author pronouns if found. Otherwise returns "null"
  */
 function getPronounField(status, accountName) {
-	debug(status);
 	// get account from status and pull out fields
 	const account = status.account;
 	const fields = account.fields;
 
 	for (const field of fields) {
 		//match fields against "pronouns"
-		//TODO: multiple languages
+		//TODO: multiple languages -> match against list
 		if (field.name.toLowerCase().includes("pronouns")) {
+			//TODO: see https://github.com/ItsVipra/ProToots/issues/28
 			debug(`${account.acct}: ${field.value}`);
 
 			cachePronouns(accountName, field.value);
