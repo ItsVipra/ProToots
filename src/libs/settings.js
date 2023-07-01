79722334c1818147d4502b80e5207f8c0ae0ae7e
ItsVipra <vivienvielva@gmail.com>
2023-07-01 15:07:15 +0200

    add option for where to show proplates

---
+++ src/libs/settings.js
@@ -0,0 +1,32 @@
+import { storage } from "webextension-polyfill";
+
+let settings;
+
+export async function getSettings() {
+	try {
+		settings = await storage.sync.get();
+	} catch {
+		//  Enable the logging automatically if we cannot determine the user preference.
+		settings = {};
+	}
+}
+
+export function isLogging() {
+	return settings.logging;
+}
+
+export function statusVisibility() {
+	return settings.statusVisibility;
+}
+
+export function notificationVisibility() {
+	return settings.notificationVisibility;
+}
+
+export function accountVisibility() {
+	return settings.accountVisibility;
+}
+
+export function conversationVisibility() {
+	return settings.conversationVisibility;
+}
